ee14874e36e141dbd1f6bac6b356d4bb1af8d800
saronassefa <assefasaron1@gmail.com>
2022-03-13 16:16:53 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main-sizes
+*
+*Return:zero
+*/
+int main(void)
+{
+printf("Size of a char: %d byte\(s\)",1);
+printf("Size of an int: %d byte\(s\)",4);
+printf("Size of a long int: %d byte\(s\)",4);
+printf("Size of a long long int: %d byte\(s\)",8);
+printf("Size of a float: %d byte\(s\)",4);
+return (0);
+}
