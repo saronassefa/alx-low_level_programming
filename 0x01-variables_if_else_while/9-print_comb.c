06f86f2f20dcad714f9886d735f46f271e2382c7
saronassefa <assefasaron1@gmail.com>
2022-03-14 12:04:24 -0700

    digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main- combination
+*
+*Return:zero
+*/
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
