27b73b780f9e070b47f33a6e9cc8c31eefc7ffe6
saronassefa <assefasaron1@gmail.com>
2022-03-14 10:26:24 -0700

    exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main- exceptions
+*
+*Return: zero)
+*/
+int main(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+{
+if (n != q && n != e)
+{
+putchar(n);
+}
+}
+putchar('\n');
+}
