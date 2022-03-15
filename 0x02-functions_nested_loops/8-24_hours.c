a4089909c5853e70d8ab3ed38e1f1934e6e087de
saronassefa <assefasaron1@gmail.com>
2022-03-15 14:20:08 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include <main.h>
+/**
+*jack_bauer-prints minutes
+*
+*Return: void
+*/
+void jack_bauer(void)
+{
+  int i;
+  for (i = 0; i < 24; i++)
+    {
+      if (i < 10)
+	{
+	  _putchar('0');
+	  _putchar(i);
+	}
+      else if (i => 10)
+	{
+	  _putchar(i);
+	}
+      _putchar(':');
+      int j;
+      for (j = 0; j < 60; j++)
+	{
+	  if (j < 10)
+	    {
+	      _putchar('0');
+	      _putchar(j);
+	    }
+	  else if (j >= 10)
+	    {
+	      _putchar(j);
+	    }
+	}
+    }
+  return (0);
+}
