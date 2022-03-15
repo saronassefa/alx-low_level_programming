a968b0d19d8973b93d17241c6b2984f24e813de1
saronassefa <assefasaron1@gmail.com>
2022-03-15 10:17:38 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,16 +4,16 @@
 *
 *Return: 1 & 0
 */
-int _islower(int c)
+int _islower(int )
 {
-  char n;
-  if (n >= 'a' && n <= 'z')
-    {
-      return (1);
-    }
-  else if (n >= 'A' && n <= 'Z')
-    {
-      return (0);
-    }
-  return (0)
-    }
+char n;
+if (n >= 'a' && n <= 'z')
+{
+return (1);
+}
+else if (n >= 'A' && n <= 'Z')
+{
+return (0);
+}
+ return (0);
+}
