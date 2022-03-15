18e791397ba521fe8a4dad6ad6de6dc1e6888a4a
saronassefa <assefasaron1@gmail.com>
2022-03-15 12:35:32 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*int_abs- absoulute value
+*
+*Return: int
+*/
+int _abs(int)
+{
+int n;
+if (n < 1)
+{
+n = -1 * n;
+return (n);
+}
+if else (n > 1)
+{
+return (n);
+}
+return (0);
+}
