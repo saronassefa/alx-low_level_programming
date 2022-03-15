1512b5a71de518376180db39b0c6abfd260bf6ef
saronassefa <assefasaron1@gmail.com>
2022-03-15 09:12:20 -0700

    lowerletters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main-alphabet print
+*
+*Return:0
+*/
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz"
+int i;
+for (i = 0; i < 26; i++)
+{
+_putchar(alpha[i]);
+}
+_putchar('\n');
+return (0);
