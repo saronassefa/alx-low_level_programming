c419ee2500dbafd4722d6f5c456e2a189f5730d8
saronassefa <assefasaron1@gmail.com>
2022-03-15 10:39:22 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_isalpha- checks alpha
+*@c: ascii
+*
+*Return: zero and one
+*/
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
