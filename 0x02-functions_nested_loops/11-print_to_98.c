3fc04faae02a115a4991e67f5b03e48697d59e48
saronassefa <assefasaron1@gmail.com>
2022-03-16 08:07:41 -0700

    print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,20 @@ void print_to_98(int n)
 {
   while (n >= 98)
     {
-      _putchar(n);
+      putchar(n);
       if (n > 98)
 	{
-      _putchar(',');
+      putchar(',');
 	}
       n--;
     }
 
   while (n <=98)
     {
-      _putchar(n);
+      putchar(n);
       if (n < 98)
 	{
-      _putchar(',');
+      putchar(',');
 	}
       n++;
     }
