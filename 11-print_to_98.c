31c846128effbf838b5803166da598b21f5be57d
saronassefa <assefasaron1@gmail.com>
2022-03-16 09:54:25 -0700

    98

---
+++ 11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*print_to_98- main
+*@n:int
+*
+*Return: void
+*/
+void print_to_98(int n)
+{
+  while (n >= 98)
+    {
+      printf("%d",n);
+      if (n > 98)
+	{
+      printf(", ");
+	}
+      n--;
+    }
+
+  while (n <= 98)
+    {
+      printf("%d",n);
+      if (n < 98)
+	{
+      printf(", ");
+	}
+      n++;
+    }
+  return;
+}
