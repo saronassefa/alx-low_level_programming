ba0d168149e96ed6c559ed08aba254cdd738a375
saronassefa <assefasaron1@gmail.com>
2022-03-16 07:22:49 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add- adds
+*
+*Return:0
+*/
+int add(int,int)
+{
+  int num = a + b;
+  return (num);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*times_table- prints times table
+*
+*Return: void
+*/
+void times_table(void)
+{
+  for(n = 0; n < 9; n++)
+    {
+      int i;
+      for (i = 0; i < 1; i++) 
+	{
+	  
+	  _putchar('0');
+	  _putchar(',');
+	}
+     
