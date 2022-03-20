77129492dea53f090f08a6b30ee4b85ff4daf22e
saronassefa <assefasaron1@gmail.com>
2022-03-20 16:22:14 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
 */
 int _isupper(int c)
 {
-  for (c = 'A'; c <= 'Z'; c++)
-    {
-      return (1);
-    }
-  for (c ='a'; c <= 'z'; c++)
-    {
-      return (0); 
-    }
-  }
+if (c < 91 && c > 64)
+{
+return (1);
+}
+else
+{
+return (0); 
+}
+}
