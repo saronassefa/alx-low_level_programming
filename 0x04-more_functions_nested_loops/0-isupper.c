95492192f7c85681e28fd4567f6f05af64a6b7fb
saronassefa <assefasaron1@gmail.com>
2022-03-20 16:16:55 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper- uppercase
+*@c:int
+*Return:int
+*/
+int _isupper(int c)
+{
+  for (c = 'A'; c <= 'Z'; c++)
+    {
+      return (1);
+    }
+  for (c ='a'; c <= 'z'; c++)
+    {
+      return (0); 
+    }
+  }
