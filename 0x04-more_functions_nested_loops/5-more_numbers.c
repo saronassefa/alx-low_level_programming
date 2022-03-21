ffb9043f44767c40437009c1427463186e720b3d
saronassefa <assefasaron1@gmail.com>
2022-03-20 17:45:08 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,10 +5,9 @@
 */
 void more_numbers(void)
 {
-int i;
+  int i,j;
 for (i = 0; i <= 10; i++)
 {
-int j;
 for (j = 0; j <= 14; j++)
 {
 if (j > 9)
@@ -18,4 +17,3 @@ _putchar((j % 10) + '0');
 _putchar('\n');
 }
 }
-
