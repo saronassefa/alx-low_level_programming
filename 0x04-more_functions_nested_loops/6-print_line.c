d6f88b101e031b4b11b50989872517aef01b2248
saronassefa <assefasaron1@gmail.com>
2022-03-20 18:02:49 -0700

    -

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line- line print
+*@n: int
+*Return:void
+*/
+void print_line(int n)
+{
+int i = 0;
+while (i < 0)
+{
+if ( n > 0)
+{
+_putchar(95);
+}
+else
+{
+_putchar('\n');
+}
+_putchar('\n');
+}
+}
